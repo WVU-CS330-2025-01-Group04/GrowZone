e5c8861d19e3fee526a9b0e13e30cc3614547143
tjg00022 <157546838+tjg00022@users.noreply.github.com>
2025-05-06 00:30:32 -0400

    Update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -14,11 +14,11 @@ const __dirname = path.dirname(__filename);
 const caCert = fs.readFileSync(path.join(__dirname, 'DigiCertGlobalRootCA.crt.pem'));
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'authdb',
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 4002,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
