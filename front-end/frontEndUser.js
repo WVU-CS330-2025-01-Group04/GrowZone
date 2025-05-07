8eaf23d3518114f1800e2eb4a8ddf746d36d0f11
tjg00022 <157546838+tjg00022@users.noreply.github.com>
2025-05-07 00:04:47 -0400

    Update frontEndUser.js

--- front-end/frontEndUser.js
+++ front-end/frontEndUser.js
@@ -29,7 +29,7 @@ async function handleLogin(event) {
     }
 
     try {
-        const response = await fetch(`${API_URL}/`, {
+        const response = await fetch(`${API_URL}/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
